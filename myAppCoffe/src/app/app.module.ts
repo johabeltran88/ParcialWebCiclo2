3e9cf16ca3835b0ec5a6f3c057ea3368b516e7d2
Johanna Beltran <ja.beltrang12@uniandes.edu.co>
2023-04-29 18:40:13 -0500

    Punto 1 completado

--- myAppCoffe/src/app/app.module.ts
+++ myAppCoffe/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CafeModule } from './cafe/cafe.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CafeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
