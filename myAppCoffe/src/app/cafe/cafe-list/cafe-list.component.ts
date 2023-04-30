a8237d60548db75235e99e8efd04d281b369d3d3
Johanna Beltran <ja.beltrang12@uniandes.edu.co>
2023-04-29 19:01:43 -0500

    Punto 2 completo

--- myAppCoffe/src/app/cafe/cafe-list/cafe-list.component.ts
+++ myAppCoffe/src/app/cafe/cafe-list/cafe-list.component.ts
@@ -9,12 +9,40 @@ import { CafeService } from '../cafe.service';
 })
 export class CafeListComponent implements OnInit {
   cafes: Array<Cafe> = [];
+  origen: number = 0;
+  blend:  number = 0;
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
     this.cafes = cafes;
+    this.getOrigen(cafes);
+    this.getBlend(cafes);
     });
+  }
+
+  getOrigen(cafes: Array<Cafe>): void{
+    let cont: number = 0;
+
+    for(let c of this.cafes){
+      if(c.tipo.includes('Origen')){
+        cont += 1;
+      }
+
+      this.origen = cont;
+    }
+  }
+
+  getBlend(cafes: Array<Cafe>): void{
+    let cont: number = 0;
+
+    for(let c of this.cafes){
+      if(c.tipo.includes('Blend')){
+        cont += 1;
+      }
+
+      this.blend = cont;
+    }
   }
 
   ngOnInit() {
